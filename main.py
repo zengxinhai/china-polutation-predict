e908c5892ccddc0f09368e18e429ff7bf562ce77
icefox <zengxinhai001@163.com>
2023-05-19 20:33:44 +0800

    Add average age chart

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# utf-8 encoding
-
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -33,10 +31,25 @@ def extend_born_data(df, till_year):
     return df
 
 
-# Add a column to the dataframe to indicate the age of the people given the current year
-def add_age(df, current_year):
+# Calculate the average age of the population for a given year
+# Given the number of people born each year, the current year, and the death age
+# Do not modify the dataframe
+def average_age(df, current_year, death_age):
     df['Age'] = current_year - df['Year']
-    return df
+    total_age = df['Born'] * df['Age'] * (df['Age'] < death_age) * (df['Age'] >= 0)
+    total = df['Born'] * (df['Age'] < death_age) * (df['Age'] >= 0)
+    return total_age.sum() / total.sum()
+
+
+# Make a new dataframe to store the average age for each year
+# The csv format is: <Year>, <AverageAge>
+# For example: 2019, 20.5
+def create_average_age_df(df, till_year, death_age):
+    age_df = pd.DataFrame(columns=['Year', 'AverageAge'])
+    for year in range(2023, till_year):
+        ag = average_age(df, year, death_age)
+        age_df = pd.concat([age_df, pd.DataFrame([[year, ag]], columns=age_df.columns)])
+    return age_df
 
 
 # Calculate the number of people between age a and b, not including b
@@ -51,21 +64,42 @@ def num_between_age_a_and_b(df, a, b, current_year, death_age):
 # Make a new dataframe to store the number of total, worker, children, and old people for each year
 # The csv format is: <Year>, <Total>, <Worker>, <Children>, <Old>
 # For example: 2019, 100, 50, 20, 30
-def create_group_df(df, till_year, death_age):
+def create_group_df(df, till_year, worker_age, retire_age, death_age):
     group_df = pd.DataFrame(columns=['Year', 'Total', 'Worker', 'Children', 'Old'])
     for year in range(2023, till_year):
-        total = num_between_age_a_and_b(df, 0, 78, year, death_age)
-        old = num_between_age_a_and_b(df, 60, 78, year, death_age)
-        worker = num_between_age_a_and_b(df, 20, 60, year, death_age)
-        children = num_between_age_a_and_b(df, 0, 20, year, death_age)
+        total = num_between_age_a_and_b(df, 0, death_age - 1, year, death_age)
+        old = num_between_age_a_and_b(df, retire_age, death_age - 1, year, death_age)
+        worker = num_between_age_a_and_b(df, worker_age, retire_age, year, death_age)
+        children = num_between_age_a_and_b(df, 0, worker_age, year, death_age)
         group_df = pd.concat([group_df, pd.DataFrame([[year, total, worker, children, old]], columns=group_df.columns)])
     return group_df
 
 
+# constants
+worker_age = 20
+retire_age = 50
+death_age = 79
+till_year = 2100
+
+# Set the font to Chinese
+plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+
+# Prepare the data
 df = read_born_csv()
-df = extend_born_data(df, 2200)
-df = add_age(df, 2023)
-group_df = create_group_df(df, 2100, 79)
+df = extend_born_data(df, till_year)
+
+average_age_df = create_average_age_df(df, till_year, death_age)
+average_age_df = average_age_df[average_age_df['Year'] % 5 == 0]
+# plot the average age for each year
+plt.title(u'中国人口平均年龄')
+plt.xlabel('年份')
+plt.ylabel('岁')
+plt.plot(average_age_df['Year'], average_age_df['AverageAge'], marker='o', label=u'平均年龄', color='red')
+plt.legend()
+plt.show()
+
+
+group_df = create_group_df(df, till_year, worker_age, retire_age, death_age)
 # plot the ratio of worker, children, and old people for each 5 years
 # show line chart for each group
 # show the value for the marker
@@ -74,15 +108,20 @@ group_df['ChildrenRate'] = group_df['Children'] / group_df['Total']
 group_df['OldRate'] = group_df['Old'] / group_df['Total']
 group_df = group_df[group_df['Year'] % 5 == 0]
 
-plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
-# plt.title(u'中国人口数量预测')
-# plt.xlabel('年份')
-# plt.ylabel('万人')
-# plt.plot(group_df['Year'], group_df['Worker'], marker='o', label=u'劳动人口', color='blue')
-# plt.plot(group_df['Year'], group_df['Children'], marker='o', label=u'儿童青少年', color='green')
-# plt.plot(group_df['Year'], group_df['Old'], marker='o', label=u'老年人', color='orange')
-# plt.legend()
-# plt.show()
+# Plot the line chart for average age for each year
+plt.title(u'中国人口平均年龄')
+plt.xlabel('年份')
+plt.ylabel('岁')
+
+
+plt.title(u'中国人口数量预测')
+plt.xlabel('年份')
+plt.ylabel('万人')
+plt.plot(group_df['Year'], group_df['Worker'], marker='o', label=u'劳动人口', color='blue')
+plt.plot(group_df['Year'], group_df['Children'], marker='o', label=u'儿童青少年', color='green')
+plt.plot(group_df['Year'], group_df['Old'], marker='o', label=u'老年人', color='orange')
+plt.legend()
+plt.show()
 
 
 # Plot the line chart for each group
